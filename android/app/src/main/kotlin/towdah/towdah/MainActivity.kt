73999587f1caf6b5354271c88eb4f3e111d79f54
Firstname Lastname <meshy112@gmail.com>
2020-09-22 10:01:37 +0300

    first commit

---
+++ android/app/src/main/kotlin/towdah/towdah/MainActivity.kt
@@ -0,0 +1,6 @@
+package towdah.towdah
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+void main() => runApp(MaterialApp(
+  debugShowCheckedModeBanner: false,
+  home: Home()
+));
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('TOWDAH'),
+        centerTitle: true,
+        backgroundColor: Colors.blue,
+      ),
+      body: Center(
+
+
+          ),
+
+
+      floatingActionButton:FloatingActionButton(
+        child: Text('click'),
+      ) ,
+    );
+  }
+}
+
