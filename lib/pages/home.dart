0cc91db916d0ce88f8ae537bb7ebbd6a0294f551
Firstname Lastname <meshy112@gmail.com>
2020-09-28 11:11:15 +0300

    update home view

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,11 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class ScaffoldExample extends StatelessWidget {
-  meshy_() {
-    debugPrint("soon you'll be able to search for cool songs");
-  }
-
+class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -122,35 +118,5 @@ class ScaffoldExample extends StatelessWidget {
     //     ),
     // ),
     // );
-  }
-}
-
-class CustomButton extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: () {
-        final snackBar = SnackBar(
-          content: Text('Where Gospel Breathes'),
-        );
-        Scaffold.of(context).showSnackBar(snackBar);
-      },
-      child: Container(
-        padding: EdgeInsets.all(10.0),
-        decoration: BoxDecoration(borderRadius: BorderRadius.circular(8.0)),
-        child: Text('Towdah'),
-      ),
-    );
-  }
-}
-
-class Home extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-        child: Text(
-      'hello Flutter!',
-      textDirection: TextDirection.ltr,
-    ));
   }
 }
