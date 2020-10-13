8ecf87995caf973aaa8f33e6057e63eac4847a27
jonathan <jonthanmoyo123@gmail.com>
2020-10-13 00:47:24 -0700

    forgot my password

--- lib/pages/LoginForm.dart
+++ lib/pages/LoginForm.dart
@@ -11,15 +11,17 @@ class LoginForm extends StatefulWidget {
   _LoginFormState createState() => _LoginFormState();
 }
 
-class _LoginFormState extends State<LoginForm>{
-TextEditingController _passwordTextController = new TextEditingController();
-  final Map< String  , dynamic> _formData = {
+class _LoginFormState extends State<LoginForm> {
+  TextEditingController _passwordTextController = new TextEditingController();
+  final Map<String, dynamic> _formData = {
     'email': null,
     'phoneNumber': null,
     'password': null,
     'acceptTerms': false
   };
+
   final _formKey = GlobalKey <FormState>();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -30,223 +32,243 @@ TextEditingController _passwordTextController = new TextEditingController();
       //     fit: BoxFit.cover,
       //   ),
       // ),
-      body:SingleChildScrollView(
+      body: SingleChildScrollView(
 
 
-      child:Container(
+        child: Container(
 
-        // decoration: BoxDecoration(
-        //   image: DecorationImage(
-        //     image: AssetImage("assets/Artists.jpg"),
-        //     fit: BoxFit.cover,
-        //   ),
-        // ),
-
-          child:Padding(
-            padding: const EdgeInsets.fromLTRB(10.0, 0.0, 10.0, 0.0),
+          //   Container(
+          // decoration: BoxDecoration(
+          //   image: DecorationImage(
+          //     image: AssetImage("assets/Artists.jpg"),
+          //     fit: BoxFit.cover,
+          //   ),
+          //  ),
+          // ),
+          child: Padding(
+            padding: const EdgeInsets.fromLTRB(30.0, 50.0, 30.0, 0.0),
             child: Stack(
-              children: [
+                children: [
 
-             Form(
+                  Form(
 
-               key: _formKey,
-               child:Container (
-                  child: Column(
+                    key: _formKey,
+                    child: Container(
+                      child: Column(
 
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children:<Widget>[
-                      Center(
-                        child: Image.asset('assets/face.png',
-                        width:70,
-                          height:70,
-                          color:Colors.white,
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: <Widget>[
+                            Center(
+                              child: Image.asset('assets/face.png',
+                                width: 70,
+                                height: 70,
+                                color: Colors.white,
 
-                        ),
-                      ),
-                      Container(
-                        padding: EdgeInsets.all(10),
-                        child: Text('Towdah',
-                            style: TextStyle(color: Colors.teal,
-                              letterSpacing: 10,
-                              fontFamily: 'Caveat',
-                              fontWeight: FontWeight.bold,
-                                fontSize: 50
-                        ),
-                       ),
-                      ),
-                      TextFormField(
-                        decoration: InputDecoration(
-                            labelText: 'E-Mail', filled: true,),
-                        keyboardType: TextInputType.emailAddress,
-                        validator: (String value) {
-                          //begins********************
-                          if (value.isEmpty ||
-                              !RegExp(r"[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?")
-                                  .hasMatch(value)) {
-                            return 'Please enter a valid email';
-                          }
-                          return null;
-                        }, //ends******************
-                        onSaved: (String value) {
-                          _formData['email'] = value;
-                        },
-                      ),
-                      TextFormField(
-                        decoration: InputDecoration(
-                            labelText: 'Password', filled: true),
-                        obscureText: true,
-                        controller: _passwordTextController,
-                        validator: (String value) {
-                          //begins**********
-                          if (value.isEmpty || value.length < 6) {
-                            return 'Password invalid,should have more than 6 characters';
-                          }
-                          return null;
-                        }, //end********************
-                        onSaved: (String value) {
-                          _formData['password'] = value;
-                        },
-                      ),
-                      FlatButton(onPressed:(){
-                        print("you pressed this button");
-                        if(_formKey.currentState.validate()){
-                          _formKey.currentState.save();
-                          validateAndSubmit();
-                          Navigator.push(context, MaterialPageRoute(
-                              builder: (context) => Home()));
-                        }
-                      },
-                          color: Colors.teal,
-                          textColor: Colors.grey[700],
-                          child: Text('Login'),
-                          shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.all(
-                          Radius.circular(20)))
-
-                      ),
-
-                      //other signing in options
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Container(
-                            padding: EdgeInsets.only(top: 15, left: 20),
-                            child: InkWell(
-
-                              child: Text('Forgot Password',
-                                style: TextStyle(
-                                    color: Colors.teal,
-                                    fontWeight: FontWeight.bold,
-                                    fontFamily: 'Montserrat',
-                                    decoration: TextDecoration.underline
-                                ),),
+                              ),
                             ),
-                          ),
-                          Container(
-                            padding: EdgeInsets.only(top: 15, left: 20),
-                            child: InkWell(
-
-                              child: Text('sing in with google',
-                                style: TextStyle(
-                                    color: Colors.teal,
+                            Container(
+                              padding: EdgeInsets.all(10),
+                              child: Text('Towdah',
+                                style: TextStyle(color: Colors.teal,
+                                    letterSpacing: 10,
+                                    fontFamily: 'Caveat',
                                     fontWeight: FontWeight.bold,
-                                    fontFamily: 'Montserrat',
-                                    decoration: TextDecoration.underline
+                                    fontSize: 50
                                 ),
                               ),
-                              onTap:(){
-                                signInWithGoogle().then((result) {
-                                  if (result != null) {
-                                    Navigator.of(context).push(
-                                        MaterialPageRoute(
-                                            builder: (context) => Home()));
-                                  }
-                                });
+                            ),
+                            TextFormField(
+                              decoration: InputDecoration(
+                                labelText: 'E-Mail', filled: true,),
+                              keyboardType: TextInputType.emailAddress,
+                              validator: (String value) {
+                                //begins********************
+                                if (value.isEmpty ||
+                                    !RegExp(
+                                        r"[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?")
+                                        .hasMatch(value)) {
+                                  return 'Please enter a valid email';
+                                }
+                                return null;
+                              }, //ends******************
+                              onSaved: (String value) {
+                                _formData['email'] = value;
                               },
                             ),
-                          ),
-                        ],
-                      ),
-
-                      //go to create account
-                      SizedBox(height: 10),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: <Widget>[
-                          Center(
-                            child: Text('New to Towdah?',
-                              style: TextStyle(fontFamily: 'Montserrat',
-                                  fontWeight: FontWeight.bold),),
-                          ),
-                          SizedBox(width: 5),
-                          InkWell(
-                            onTap: () {
-                              Navigator.push(context, MaterialPageRoute(
-                                  builder: (context) => RegisterPage()));
+                            TextFormField(
+                              decoration: InputDecoration(
+                                  labelText: 'Password', filled: true),
+                              obscureText: true,
+                              controller: _passwordTextController,
+                              validator: (String value) {
+                                //begins**********
+                                if (value.isEmpty || value.length < 6) {
+                                  return 'Password invalid,should have more than 6 characters';
+                                }
+                                return null;
+                              },
+                              //end********************
+                              onSaved: (String value) {
+                                _formData['password'] = value;
+                              },
+                            ),
+                            FlatButton(onPressed: () {
+                              print("you pressed this button");
+                              if (_formKey.currentState.validate()) {
+                                _formKey.currentState.save();
+                                validateAndSubmit();
+                                Navigator.push(context, MaterialPageRoute(
+                                    builder: (context) => Home()));
+                              }
                             },
+                                color: Colors.teal,
+                                textColor: Colors.grey[700],
+                                child: Text('Login'),
+                                shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.all(
+                                        Radius.circular(20)))
 
-                            child: Text('Sign Up!',
-                              style: TextStyle(
-                                  color: Colors.teal,
-                                  fontFamily: 'Montserrat',
-                                  fontWeight: FontWeight.bold,
-                                  decoration: TextDecoration.underline
-                              ),),
-                          )
+                            ),
 
-                        ],
+                            //other signing in options
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Container(
+                                  padding: EdgeInsets.only(top: 15, left: 20),
+                                  child: InkWell(
+
+                                    child: Text('Forgot Password',
+                                      style: TextStyle(
+                                          color: Colors.teal,
+                                          fontWeight: FontWeight.bold,
+                                          fontFamily: 'Montserrat',
+                                          decoration: TextDecoration.underline
+                                      ),),
+                                    onTap: () {
+                                      resetPassword();
+                                    },
+                                  ),
+                                ),
+                                Container(
+                                  padding: EdgeInsets.only(top: 15, left: 20),
+                                  child: InkWell(
+
+                                    child: Text('sing in with google',
+                                      style: TextStyle(
+                                          color: Colors.teal,
+                                          fontWeight: FontWeight.bold,
+                                          fontFamily: 'Montserrat',
+                                          decoration: TextDecoration.underline
+                                      ),
+                                    ),
+                                    onTap: () {
+                                      signInWithGoogle().then((result) {
+                                        if (result != null) {
+                                          Navigator.of(context).push(
+                                              MaterialPageRoute(
+                                                  builder: (context) =>
+                                                      Home()));
+                                        }
+                                      });
+                                    },
+                                  ),
+                                ),
+                              ],
+                            ),
+
+                            //go to create account
+                            SizedBox(height: 10),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: <Widget>[
+                                Center(
+                                  child: Text('New to Towdah?',
+                                    style: TextStyle(fontFamily: 'Montserrat',
+                                        fontWeight: FontWeight.bold),),
+                                ),
+                                SizedBox(width: 5),
+                                InkWell(
+                                  onTap: () {
+                                    Navigator.push(context, MaterialPageRoute(
+                                        builder: (context) => RegisterPage()));
+                                  },
+
+                                  child: Text('Sign Up!',
+                                    style: TextStyle(
+                                        color: Colors.teal,
+                                        fontFamily: 'Montserrat',
+                                        fontWeight: FontWeight.bold,
+                                        decoration: TextDecoration.underline
+                                    ),),
+                                )
+
+                              ],
+                            ),
+
+                          ]
                       ),
-
-                    ]
-                  ),
-               ),
-             )
-              ]
+                    ),
+                  )
+                ]
             ),
           ),
 
-             ),
+
+        ),
       ),
     );
-          }
-             //user signing in
-          Future validateAndSubmit()async {
-            FirebaseUser user =await _auth.signInWithEmailAndPassword(email:  _formData["email"].trim(), password:_formData["password"].trim()  )
-                .then((user) {
-              print("your in already ${user.credential}");
-              //print("Email:${user}")
-            });
-          }
+  }
 
-          // google signing in
-          Future<String> signInWithGoogle()async {
+  //user signing in
+  Future validateAndSubmit() async {
+    FirebaseUser user = await _auth.signInWithEmailAndPassword(
+        email: _formData["email"].trim(),
+        password: _formData["password"].trim())
+        .then((user) {
+      print("your in already ${user.credential}");
+      //print("Email:${user}")
+    }
+    );
+  }
 
-            await Firebase.initializeApp();
+  // google signing in
+  Future<String> signInWithGoogle() async {
+    await Firebase.initializeApp();
 
-            final GoogleSignInAccount googleSignInAccount = await googleSignIn.signIn();
-            final GoogleSignInAuthentication googleSignInAuthentication = await googleSignInAccount.authentication;
+    final GoogleSignInAccount googleSignInAccount = await googleSignIn.signIn();
+    final GoogleSignInAuthentication googleSignInAuthentication = await googleSignInAccount
+        .authentication;
 
-            final AuthCredential credential = GoogleAuthProvider.credential(
-              accessToken: googleSignInAuthentication.accessToken,
-              idToken: googleSignInAuthentication.idToken,
-            );
+    final AuthCredential credential = GoogleAuthProvider.credential(
+      accessToken: googleSignInAuthentication.accessToken,
+      idToken: googleSignInAuthentication.idToken,
+    );
 
-            final UserCredential authResult = await _auth.signInWithCredential(credential);
-            final User user = authResult.user;
+    final UserCredential authResult = await _auth.signInWithCredential(
+        credential);
+    final User user = authResult.user;
 
-            if (user != null) {
-              assert(!user.isAnonymous);
-              assert(await user.getIdToken() != null);
+    if (user != null) {
+      assert(!user.isAnonymous);
+      assert(await user.getIdToken() != null);
 
-              final User currentUser = _auth.currentUser;
-              assert(user.uid == currentUser.uid);
+      final User currentUser = _auth.currentUser;
+      assert(user.uid == currentUser.uid);
 
-              print('signInWithGoogle succeeded: $user');
+      print('signInWithGoogle succeeded: $user');
 
-              return '$user';
-            }
+      return '$user';
+    }
 
-              return null;
-            }
+    return null;
+  }
 
+  Future <void> resetPassword() async {
+    if (_formData["email"].trim() != null) {
+      await _auth.sendPasswordResetEmail(email: _formData["email"]);
+    }else {
+      return "please enter your email";
+    }
+  }
         }
